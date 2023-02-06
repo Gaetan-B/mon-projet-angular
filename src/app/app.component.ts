8695452d28857de7ce1e9e5f0179e021fc4088e4
Gaetan-B <78168196+Gaetan-B@users.noreply.github.com>
2023-02-06 16:24:38 +0100

    bug
    bug routeur 1h21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,42 +6,9 @@ import {AppareilService} from "./services/appareil.services";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  isAuth = false;
-
-  lastUpdate = new Promise<Date>(
-    (resolve, reject) => {
-      const date = new Date();
-      setTimeout(
-        () => {
-          resolve(date);
-        }, 2000
-      )
-    }
-  )
-
- appareils: any[] | undefined;
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth =true;
-      }, 4000
-    );
-
-  }
-
-  ngOnInit() {
-      this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-      this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-      this.appareilService.switchOffAll();
-  }
+export class AppComponent {
+constructor() {
+}
 
 }
 
